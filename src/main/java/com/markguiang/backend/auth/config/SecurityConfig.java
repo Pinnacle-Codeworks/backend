2ebf772a800bc6b17bf1813b6fb910ec291dcc17
Mark Guiang <mahikgot@gmail.com>
2025-03-09 23:47:36 +0800

    feat: add csrf protection

--- src/main/java/com/markguiang/backend/auth/config/SecurityConfig.java
+++ src/main/java/com/markguiang/backend/auth/config/SecurityConfig.java
@@ -8,13 +8,13 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.DelegatingSecurityContextRepository;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 
 @Configuration
@@ -23,16 +23,22 @@ import org.springframework.security.web.context.RequestAttributeSecurityContextR
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/auth/user").permitAll()
-            .anyRequest().authenticated())
-            .csrf(AbstractHttpConfigurer::disable)
-            .securityContext((securityContext) -> securityContext
-            .securityContextRepository(new DelegatingSecurityContextRepository(
-                    new RequestAttributeSecurityContextRepository(),
-                    new HttpSessionSecurityContextRepository()
-            ))
-        );
+        XorCsrfTokenRequestAttributeHandler requestHandler = new XorCsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf((csrf) -> csrf
+                        .csrfTokenRequestHandler(requestHandler)
+                )
+                .securityContext((securityContext) -> securityContext
+                        .securityContextRepository(new DelegatingSecurityContextRepository(
+                                new RequestAttributeSecurityContextRepository(),
+                                new HttpSessionSecurityContextRepository()
+                        ))
+                );
         return http.build();
     }
 
