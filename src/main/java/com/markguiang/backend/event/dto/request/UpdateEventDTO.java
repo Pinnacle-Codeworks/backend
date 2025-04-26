69025e58203bdfe781ea0c2d31fb1c1a225eb2f0
julianabot <julianarolluqui@gmail.com>
2025-04-26 19:48:03 +0800

    refactor: change event model columns

--- src/main/java/com/markguiang/backend/event/dto/request/UpdateEventDTO.java
+++ src/main/java/com/markguiang/backend/event/dto/request/UpdateEventDTO.java
@@ -4,19 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.markguiang.backend.annotation.ValidRequired;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 
 public record UpdateEventDTO(
         @NotNull Long eventId,
         @NotNull String name,
         String description,
-        Calendar dateTime,
         String location,
         Boolean hasMultipleLocation,
-        Map<LocalDate, String> dateLocationMap,
         @JsonProperty("scheduleList")
         @ValidRequired
         List<UpdateEventScheduleDTO> updateEventScheduleDTOList
@@ -26,5 +22,6 @@ public record UpdateEventDTO(
             @NotNull Calendar startDate,
             @NotNull Calendar endDate,
             String location
-    ) {}
+    ) {
+    }
 }
--- src/main/java/com/markguiang/backend/event/model/Event.java
+++ src/main/java/com/markguiang/backend/event/model/Event.java
@@ -1,19 +1,12 @@
 package com.markguiang.backend.event.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.key.LocalDateKeyDeserializer;
 import com.markguiang.backend.base.AbstractBaseEntity;
 import jakarta.persistence.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
-import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 
 @Entity
-@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "tenantId", "name" }) })
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"tenantId", "name"})})
 public class Event extends AbstractBaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,24 +16,12 @@ public class Event extends AbstractBaseEntity {
     @Column(nullable = false)
     private String name;
     private String description;
-    private Calendar dateTime;
     private String location;
     private Boolean hasMultipleLocation;
-    @JdbcTypeCode(SqlTypes.JSON)
-    @JsonDeserialize(keyUsing = LocalDateKeyDeserializer.class)
-    private Map<LocalDate, String> dateLocationMap;
 
     // children
     @Transient
     private List<Schedule> scheduleList;
-
-    public Map<LocalDate, String> getDateLocationMap() {
-        return dateLocationMap;
-    }
-
-    public void setDateLocationMap(Map<LocalDate, String> dateLocationMap) {
-        this.dateLocationMap = dateLocationMap;
-    }
 
     public Boolean getHasMultipleLocation() {
         return hasMultipleLocation;
@@ -56,14 +37,6 @@ public class Event extends AbstractBaseEntity {
 
     public void setLocation(String location) {
         this.location = location;
-    }
-
-    public Calendar getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(Calendar dateTime) {
-        this.dateTime = dateTime;
     }
 
     public String getDescription() {
