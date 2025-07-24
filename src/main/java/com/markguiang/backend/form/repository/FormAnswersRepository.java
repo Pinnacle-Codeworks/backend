d54dd4209c5caa9f6a3d7f4b93b5f3e3cf7f912a
Mark Guiang <mahikgot@gmail.com>
2025-07-24 23:23:47 +0800

    refactor: delete form

--- src/main/java/com/markguiang/backend/form/repository/FormAnswersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.markguiang.backend.form.repository;
-
-import com.markguiang.backend.form.model.FormAnswers;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FormAnswersRepository extends CrudRepository<FormAnswers, Long> {
-}
--- src/main/java/com/markguiang/backend/form/repository/FormRepository.java
+++
@@ -1,7 +0,0 @@
-package com.markguiang.backend.form.repository;
-
-import com.markguiang.backend.form.model.Form;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FormRepository extends CrudRepository<Form, Long> {
-}
