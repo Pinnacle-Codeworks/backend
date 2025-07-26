fcd8c0acf468635f6b18974e9961d980bd60062d
Mark Guiang <mahikgot@gmail.com>
2025-07-26 23:17:14 +0800

    fix: remove hardcoded servicekeyaccount json

--- src/main/java/com/markguiang/backend/infrastructure/auth/config/FirebaseInitializer.java
+++ src/main/java/com/markguiang/backend/infrastructure/auth/config/FirebaseInitializer.java
@@ -3,22 +3,25 @@ package com.markguiang.backend.infrastructure.auth.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FirebaseInitializer {
+  @Value("${SERVICE_KEY_JSON}")
+  private String serviceKey;
+
   @PostConstruct
   public void init() {
     try {
-      FileInputStream serviceAccount =
-          new FileInputStream("src/main/resources/serviceKeyAccount.json");
-
-      FirebaseOptions options =
-          FirebaseOptions.builder()
-              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-              .build();
+      InputStream serviceAccount = new ByteArrayInputStream(serviceKey.getBytes(StandardCharsets.UTF_8));
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .build();
 
       if (FirebaseApp.getApps().isEmpty()) {
         FirebaseApp.initializeApp(options);
